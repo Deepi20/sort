3a1dd8af9e6330405b873aaa045c7ec36cc6d471
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-06-14 23:56:28 -0500

    Update comparator.py

--- comparator.py
+++ comparator.py
@@ -28,3 +28,13 @@ class Player:
         return 0
 
 n = int(raw_input())
+data = []
+for i in range(n):
+    name, score = raw_input().split()
+    score = int(score)
+    player = Player(name, score)
+    data.append(player)
+    
+data = sorted(data, cmp=Player.comparator)
+for i in data:
+    print i.name, i.score
