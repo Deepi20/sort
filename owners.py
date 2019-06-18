506153245bc502e214929d6ad590f8102cb8a96a
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-06-17 23:16:19 -0500

    Create owners.py

---
+++ owners.py
@@ -0,0 +1,21 @@
+class FileOwners:
+
+    @staticmethod
+
+    def group_by_owners(files):
+        owners=set()
+        ownerDict={}
+
+        for k, v in files.items():
+            #fill set w/ names of owners
+            owners.add(v)
+        for o in owners:
+            ownerDict[o]=[]
+            for k, v in files.items():
+                #fill out new dictionary w/ list of file names
+                if v==o:
+                    ownerDict[o].append(k)
+        return ownerDict;
+
+
+
